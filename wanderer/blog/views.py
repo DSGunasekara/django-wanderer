90af316ee526e67d767c38b8bf1f67aa28cc331e
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-05 10:16:30 +0530

    passed data to home

--- wanderer/blog/views.py
+++ wanderer/blog/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Dilain',
+        'title': 'blog 1',
+        'content': 'this is the first blog',
+        'date_posted': 'January 5, 2020'
+    },
+    {
+        'author': 'Gunasekara',
+        'title': 'blog 2',
+        'content': 'this is the second blog',
+        'date_posted': 'January 6, 2020'
+    }
+
+]
 
 
 def index(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)

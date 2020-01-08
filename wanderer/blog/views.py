6d372892eb4c6642112cf834cf6106d9acefb8eb
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-08 09:59:54 +0530

    created models and admin page

--- wanderer/blog/views.py
+++ wanderer/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 
 posts = [
     {
@@ -19,6 +20,6 @@ posts = [
 
 def index(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)

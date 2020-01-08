f9d52d02363676dba54ea871782ae3fe503cb904
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-08 21:05:15 +0530

    Changed admin panel

--- wanderer/blog/admin.py
+++ wanderer/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
+admin.site.site_header = 'Wanderer Admin'
+admin.site.site_title = 'Wanderer Admin Area'
+admin.site.index_title = 'Welcome to the Wanderer Admin Area'
+
+
 admin.site.register(Post)

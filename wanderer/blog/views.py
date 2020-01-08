d7c748a757a6ed02c9a8ebd153027a99785b1d96
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-08 10:42:02 +0530

    created users app

--- wanderer/blog/views.py
+++ wanderer/blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Dilain',
-        'title': 'blog 1',
-        'content': 'this is the first blog',
-        'date_posted': 'January 5, 2020'
-    },
-    {
-        'author': 'Gunasekara',
-        'title': 'blog 2',
-        'content': 'this is the second blog',
-        'date_posted': 'January 6, 2020'
-    }
-
-]
-
 
 def index(request):
     context = {

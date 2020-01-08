008a9ee6fcd34d39af389e18092ce183ffcf60e6
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-08 23:13:59 +0530

    Post can be update

--- wanderer/blog/urls.py
+++ wanderer/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]

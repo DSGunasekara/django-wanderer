11122ba7fbce301d9852d1142547ea869f3a47c2
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-01-21 21:18:19 +0530

    Added static root

--- wanderer/wanderer/settings.py
+++ wanderer/wanderer/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = 'so@fb@4%!i(%8)s8+5qpf28j#a$&1xe3@406*nu@al7vce#btt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secure-meadow-69972.herokuapp.com']
 
 
 # Application definition
